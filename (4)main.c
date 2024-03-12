41892f57644d446f72ee7f67a3c55c32a1c5c94d
propby <77524185+propby@users.noreply.github.com>
2024-03-12 20:32:13 +0700

    Update (4)main.c

--- (4)main.c
+++ (4)main.c
@@ -6,25 +6,25 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     unsigned char bit;
-    printf ("Íà ÷òî ïîìåíÿòü òðåòüèé áèò (1 èëè 0) ?:");
+    printf ("Change bit to what? (1 или 0) ?:");
     scanf("%d",&bit);
     unsigned int num;
-    printf ("×èñëî:");
+    printf ("Enter number:");
     scanf("%d",&num);
 
       switch (bit)
       {
       case 0:
-        num-=4;
+        num = num & (~(1<<2));
         break;
       case 1:
-        num+=4;
+        num = num | (1<<2) ;
         break;
       default:
         return 0;
       }
 
-    printf ("Èçìåíåíîå ÷èñëî:");
+    printf ("answer:");
     printf ("%d",num);
     return 0;
 }
